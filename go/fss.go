f9bf1ba161429c05f63b4eacc1230bf7b169778d
Cathie Yun <cath_yun@mit.edu>
2016-12-25 20:46:49 -0500

    Implemented generate function, except lines 12, 13, 15

--- go/fss.go
+++ go/fss.go
@@ -13,14 +13,14 @@ const N int = 64
 const AES_SIZE int = 16
 
 type CWEq struct {
-  scw [16]byte
+  scw []byte
   tlcw byte
   trcw byte
 }
 
 type ServerKeyEq struct {
   s []byte
-  t []byte 
+  t byte 
   cw []CWEq // should be length n
 }
 
@@ -68,8 +68,13 @@ func generateTreeEq(a, b uint64) (*ServerKeyEq, *ServerKeyEq) {
   rand.Read(s0)
   rand.Read(s1)
   ttemp, _ := rand.Int(rand.Reader, big.NewInt(2))
-  t0 := ttemp.Int64() & 1
+  t0 := byte(ttemp.Int64()) & 1
   t1 := t0 ^ 1
+
+  k0.s = s0
+  k1.s = s1
+  k0.t = t0
+  k1.t = t1
 
   keys := make([][]byte, 3)
   for i := range keys {
@@ -83,41 +88,42 @@ func generateTreeEq(a, b uint64) (*ServerKeyEq, *ServerKeyEq) {
 
     a_i := getBit(a, uint(i))
     fmt.Printf("a_i: %x\n", a_i)
-    // if a_i = 1: keep = r, lose = l
-    offset := 0
-    // if a_i = 0: keep = l, lose = r    
-    if (a_i) == 0 {
-      offset = 17
+    // if a_i = 0: keep = l, lose = r
+    keep := 0
+    lose := 17
+    // else: keep = r, lose = l   
+    if (a_i == 1) {
+      keep = 17
+      lose = 0
     } 
-    fmt.Printf("offset: %i\n", offset)
-
-    for k:=0; k<16; k++ {
+    k0.cw[i].scw = make([]byte, AES_SIZE)
+    k1.cw[i].scw = make([]byte, AES_SIZE)
+    for j:=0; j<16; j++ {
       // if a[i]== 0: s0r = gs0[17:33], s1r = gs1[17:33]
       // if a[i]== 1: s0l = gs0[0:16], s1l = gs1[0:16]
-      k0.cw[i].scw[k] = gs0[k+offset] ^ gs1[k+offset]
+      k0.cw[i].scw[j] = gs0[j+lose] ^ gs1[j+lose]
+      k1.cw[i].scw[j] = gs0[j+lose] ^ gs1[j+lose]
     }
-    fmt.Printf("cw: %x\n", k0.cw[i].scw)
-    fmt.Printf("gs0: %x\n, gs1: %x\n, a[i]: %x\n", gs0[16], gs1[16], a_i)
+    fmt.Printf("scw: %x\n", k0.cw[i].scw)
+    fmt.Printf("gs0: %x\ngs1: %x\na[i]: %x\n", gs0[16], gs1[16], a_i)
+    
     // tlcw := t0L ^ t1L ^ a[i] ^ 1
     k0.cw[i].tlcw = gs0[16] ^ gs1[16] ^ byte(a_i) ^ 1
+    k1.cw[i].tlcw = gs0[16] ^ gs1[16] ^ byte(a_i) ^ 1
     // trcw := t0R ^ t1R ^ a[i]
-    k0.cw[i].trcw = gs0[33] ^ gs1[33] ^ byte(a_i) & 1
+    k0.cw[i].trcw = gs0[33] ^ gs1[33] ^ byte(a_i)
+    k1.cw[i].trcw = gs0[33] ^ gs1[33] ^ byte(a_i)
     fmt.Printf("tlcw: %x\ntrcw: %x\n", k0.cw[i].tlcw, k0.cw[i].trcw)
+  
+    // s_b^i = s_b^keep xor t_b^(i-1) for b = 0, 1
+    for k:=0; k<16; k++ {
+      s0[k] = gs0[k+keep] * k0.cw[i].scw[k]
+      s1[k] = gs1[k+keep] * k1.cw[i].scw[k]
+    }
+    // TODO: which bit get xor'ed with t? least or most? Implement.
+    t0 = gs0[16+keep] ^ t0
+    t1 = gs1[16+keep] ^ t1
   }
-/*
-    unsigned char scw = s01 ^ s1l;
-    unsigned char tlcw = t0l ^ t1l ^ a[i] ^ 1;
-    unsigned char trcw = t0r ^ t1r ^ a[i];
-    *(cw + i - 1) = scw || tlcw || trcw;
-
-    // TODO: how to save the keep/lose state? or just copy code...
-    *(s0 + i - 1) = s0(keep) ^ *(t0 + i - 1) * scw;
-    *(s1 + i - 1) = s1(keep) ^ *(t1 + i - 1) * scw;
-    *(t1 + i - 1) = t1(keep) ^ *(t1 + i - 1) * t(keep)cw;
-    *(t1 + i - 1) = t1(keep) ^ *(t1 + i - 1) * t(keep)cw;
-*/  
-
-  fmt.Printf("t1: %x\n", t1)
   return k0, k1
 }
 
